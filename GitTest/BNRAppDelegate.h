a303537f7f99c90be98d258afe2fa4fbdcaed195
jimli2020 <carolli@Carols-iMac.local>
2015-08-28 16:28:15 -0400

    Initial Commit

---
+++ GitTest/BNRAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  BNRAppDelegate.h
+//  GitTest
+//
+//  Created by Carol Li on 8/28/15.
+//  Copyright (c) 2015 Carol Li. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BNRAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ GitTest/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  GitTest
+//
+//  Created by Carol Li on 8/28/15.
+//  Copyright (c) 2015 Carol Li. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "BNRAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([BNRAppDelegate class]));
+    }
+}
